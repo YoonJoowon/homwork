e3b596c7917a8690105cf471cea609454045d80d
yoonjoowon <wndnjs6224@naver.com>
2024-02-03 15:22:36 +0900

    study

--- Level.1/핸드폰가리기.js
+++ Level.1/핸드폰가리기.js
@@ -1,5 +1,9 @@
 function solution(phone_number) {
   const maskedPart = "*".repeat(phone_number.length - 4);
+  // repeat(4) 는 4번 반복
   const lastFourDigits = phone_number.slice(-4);
+  // slice 음수는 뒤에서 부터 순서만큼 분리
+  // slice(2,4) 와 같이 중간 분리도 가능
+  // slice(4) 는 4번째 부터 끝까지 분리
   return maskedPart + lastFourDigits;
 }
