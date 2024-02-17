6687ee289524777c12bf3dd739617a7fe0d13ee5
yoonjoowon <wndnjs6224@naver.com>
2024-02-17 21:27:45 +0900

    study

--- Level.2/가장 큰 수.js
+++ Level.2/가장 큰 수.js
@@ -2,7 +2,7 @@ function solution(numbers) {
   const answer = numbers
 
     .map((numbers) => String(numbers))
-    .sort((a, b) => b + a - (a + b))
+    .sort((a, b) => ((b + a) - (a + b)))
     .join("");
 
   return answer[0] === "0" ? "0" : answer;
