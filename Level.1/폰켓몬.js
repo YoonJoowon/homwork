6d0db0b9c621fa3e8a4dba29c2217697b29c22df
yoonjoowon <wndnjs6224@naver.com>
2024-02-17 15:45:42 +0900

    study

--- Level.1/폰켓몬.js
+++ Level.1/폰켓몬.js
@@ -1,12 +1,20 @@
 function solution(nums) {
-    let answer = [];
-    let choice = nums.length/2;
+  let answer = [];
+  let choice = nums.length / 2;
 
-    nums.map(num => (
-        answer.length < choice && !answer.includes(num) ? answer.push(num) : num
-        
-    ))
-    
-    
-    return answer.length;
+  nums.map((num) =>
+    answer.length < choice && !answer.includes(num) ? answer.push(num) : num
+  );
+
+  return answer.length;
+}
+
+//2try
+function solution(nums) {
+  let max = nums.length / 2;
+  let arr = [...new Set(nums)];
+
+  console.log(arr);
+
+  return arr.length > max ? max : arr.length;
 }
