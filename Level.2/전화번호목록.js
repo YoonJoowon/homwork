bd230b412d20d24dd3758ec09d38a0923a732870
yoonjoowon <wndnjs6224@naver.com>
2024-02-18 12:33:02 +0900

    study

--- Level.2/전화번호목록.js
+++ Level.2/전화번호목록.js
@@ -1,13 +1,35 @@
-function solution(phone_book) {    
-    phone_book.sort();
-    for (let i = 0; i < phone_book.length - 1; i++) {        
-        for (let j = i + 1; j < phone_book.length; j++) {
-            if (phone_book[j].includes(phone_book[i])) {
-                return false;
-            }
-        }
+function solution(phone_book) {
+  phone_book.sort();
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    for (let j = i + 1; j < phone_book.length; j++) {
+      if (phone_book[j].includes(phone_book[i])) {
+        return false;
+      }
     }
-    return true;
+  }
+  return true;
 }
 
 //2try
+function solution(phone_book) {
+  phone_book.sort();
+
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i + 1].includes(phone_book[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+//3try
+function solution(phone_book) {
+  phone_book.sort();
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i + 1].startsWith(phone_book[i])) {
+      return false;
+    }
+  }
+  return true;
+}
