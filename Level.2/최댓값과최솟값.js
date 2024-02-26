a3c094e720bb8bade472016321a92feaed6187e9
yoonjoowon <wndnjs6224@naver.com>
2024-02-26 20:14:24 +0900

    study

--- Level.2/최댓값과최솟값.js
+++ Level.2/최댓값과최솟값.js
@@ -1,8 +1,12 @@
 function solution(s) {
   s = s.split(" ");
-  console.log(s);
+
+  // 공백 기준으로 자름
+
   let max = Math.max(...s);
   let min = Math.min(...s);
 
   return min + " " + max;
+
+  // 최댓값 최솟값 Math.max min
 }
