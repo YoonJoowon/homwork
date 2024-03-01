2bf2f93d430e7ccb70714dccd36044e4104b62ba
YoonJooWon <wndnjs6224@naver.com>
2024-03-01 16:34:13 +0900

    study

--- Level.2/주식가격.js
+++ Level.2/주식가격.js
@@ -6,7 +6,9 @@ function solution(prices) {
 
     for (let j = i + 1; j < prices.length; j++) {
       stack++;
+      // 다음 주식 공지 전까지 무조건 1초는 버틴걸로 보기 때문에 stack 을 ++해줘야함
       if (prices[i] > prices[j]) {
+        // 그래서 조건을 가격이 나려갔으면 break 끊어줘야함
         break;
       }
     }
